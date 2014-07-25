bfbe005c5f7bcdca21e09f136763b147534768ff
Hit Schueler <hit.schueler@gmx.at>
2014-07-25 18:35:14 +0200

    Bruch: include __invert__

--- Bruch/bruch/Bruch.py
+++ Bruch/bruch/Bruch.py
@@ -15,14 +15,14 @@ class Bruch(object):
         self.count = 0
         self.liste = (self.zaehler,self.nenner)  
         return self
-
+    #----------------------------------------------------------------------
     def __next__(self):
         ''' receive the next value ''' 
         if self.count == len(self.liste):
             raise StopIteration
         self.count +=1
         return self.liste[self.count - 1]
-
+    #----------------------------------------------------------------------
     def __init__(self, zaehler,nenner=1):
         '''
         Constructor
@@ -38,35 +38,31 @@ class Bruch(object):
             raise ZeroDivisionError
         self.zaehler=zaehler
         self.nenner=nenner
-            
+    #----------------------------------------------------------------------            
     def __neg__(self):
         ''' -Bruch '''
         return Bruch(-self.zaehler,self.nenner)
-    
+    #----------------------------------------------------------------------    
     def __radd__(self,zaehler):
         '''
         the r-version
         '''
         return self.__add__(zaehler)
-
+    #----------------------------------------------------------------------
     def __add__(self,zaehler):
         '''
         overloading +
         '''
         if isinstance(zaehler, Bruch):
-#             z2=zaehler.zaehler
-#             n2=zaehler.nenner
             z2,n2=zaehler
         elif type(zaehler) is int:
-#             z2=zaehler
-#             n2=1
             z2,n2=zaehler,1
         else:
             raise TypeError('incompatible types:'+type(zaehler).__name__+' + Bruch()')        
         nennerNeu=self.nenner*n2
         zaehlerNeu=z2*self.nenner+n2*self.zaehler
         return Bruch(zaehlerNeu,nennerNeu)
-    
+    #----------------------------------------------------------------------    
     def __rsub__(self,left):
         '''
         the r-version
@@ -79,18 +75,19 @@ class Bruch(object):
         else:
             raise TypeError('incompatible types:'+type(left).__name__+' - Bruch()')
             
-            
+    #----------------------------------------------------------------------            
     def __sub__(self,zaehler):
         '''
         overloading -
         '''
         return self.__add__(zaehler*-1)
+    #----------------------------------------------------------------------
     def __rmul__(self,zaehler):
         '''
         the r-version
         '''
         return self.__mul__(zaehler)
-    
+    #----------------------------------------------------------------------    
     def __mul__(self,zaehler):
         '''
         overloading *
@@ -104,6 +101,7 @@ class Bruch(object):
         z2*=self.zaehler
         n2*=self.nenner
         return Bruch(z2,n2)
+    #----------------------------------------------------------------------
     def __pow__(self,p):
         '''
         Bruch()^p
@@ -113,7 +111,7 @@ class Bruch(object):
         else:
             raise TypeError('incompatible types:'+type(p).__name__+' should be an int')
              
-    
+    #----------------------------------------------------------------------    
     def __rtruediv__(self,left):
         '''
         the r-version
@@ -126,10 +124,7 @@ class Bruch(object):
         else:
             raise TypeError('incompatible types:'+type(left).__name__+' / Bruch()')
         
-#    def __div__(self,zaehler):
-#        overloading / prior py2.1
-#       return self.__truediv__(zaehler)
-        
+    #----------------------------------------------------------------------        
     def __truediv__(self,zaehler):
         '''
         overloading /
@@ -143,15 +138,19 @@ class Bruch(object):
         if z2==0:
             raise ZeroDivisionError
         return self.__mul__(Bruch(n2,z2))    
-
-
+    #----------------------------------------------------------------------
+    def __invert__(self):
+        """override ~ and invert the bruch"""
+        return Bruch(self.nenner,self.zaehler)
+        
+    #----------------------------------------------------------------------
     def eval(self):
         '''
         evaluate
         '''
         real=self.zaehler/self.nenner
         return real
-        
+    #----------------------------------------------------------------------        
     def __str__(self):
         '''
         String Repraesentation
@@ -169,6 +168,7 @@ class Bruch(object):
             return "({:d})".format(self.zaehler)
         else:    
             return "({:d}/{:d})".format(self.zaehler, self.nenner)
+    #----------------------------------------------------------------------
     @classmethod    
     def _makeBruch(other):
         '''create a Bruch from int or return the reference'''
@@ -186,31 +186,31 @@ class Bruch(object):
         '''override == '''        
         other=Bruch._makeBruch(other)
         return self.zaehler*other.nenner == other.zaehler*self.nenner
-    
+    #----------------------------------------------------------------------    
     def __ne__ (self, other):
         '''override !='''        
         return not self.__eq__(other)
-
+    #----------------------------------------------------------------------
     def __gt__ (self, other):
         '''override >'''        
         other=Bruch._makeBruch(other)
         return self.zaehler*other.nenner > other.zaehler*self.nenner
-    
+    #----------------------------------------------------------------------    
     def __lt__ (self, other):
         '''override <'''         
         other=Bruch._makeBruch(other)
         return self.zaehler*other.nenner < other.zaehler*self.nenner
-    
+    #----------------------------------------------------------------------    
     def __ge__ (self, other):
         '''override >='''        
         other=Bruch._makeBruch(other)
         return self.zaehler*other.nenner >= other.zaehler*self.nenner
-    
+    #----------------------------------------------------------------------    
     def __le__ (self, other):
         '''override <='''        
         other=Bruch._makeBruch(other)
         return self.zaehler*other.nenner <= other.zaehler*self.nenner
-
+    #----------------------------------------------------------------------
     def __abs__(self):
         '''override abs(Bruch)'''
         return Bruch(abs(self.zaehler),abs(self.nenner))
@@ -223,25 +223,25 @@ class Bruch(object):
         other=Bruch._makeBruch(other)
         self=self+other
         return self
-    
+    #----------------------------------------------------------------------    
     def __isub__(self,other):
         '''override -='''
         other=Bruch._makeBruch(other)
         self=self-other
         return self
-    
+    #----------------------------------------------------------------------    
     def __imul__(self,other):
         '''override *='''
         other=Bruch._makeBruch(other)
         self=self*other
         return self
-    
+    #----------------------------------------------------------------------    
     def __itruediv__(self,other):
         '''override /='''
         other=Bruch._makeBruch(other)
         self=self/other
         return self
-    
+    #----------------------------------------------------------------------    
     @classmethod
     def gcd(x,y):
         '''
